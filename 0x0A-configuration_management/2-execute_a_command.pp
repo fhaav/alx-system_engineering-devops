92987feeb5b080e936df2d2196b9f2b5b7b73c9a
fhaav <mzfavvy20@gmail.com>
2023-08-25 16:50:13 -0700

    Task 2 completed

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Description: This manifest kills a process named "killmenow".
+
+exec { 'kill_killmenow':
+  command => 'pkill killmenow',
+  path    => '/usr/bin:/bin',
+  onlyif  => 'pgrep killmenow',
+}
