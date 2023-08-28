d2a14f75abbbb30880de9baf3fecdd53c3cb3cc5
fhaav <mzfavvy20@gmail.com>
2023-08-28 15:58:14 -0700

    100-puppet_ssh_config.pp completed

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,12 @@
-# Puppet manifest to create an SSH config file
-$home = $facts['home']
-file { '/root/.ssh/config':
-  ensure  => 'file',
-  path    => "/root/.ssh/config",
-  mode    => '0600',
-  content => "\
-# SSH configuration for remote host
-host 34.203.201.237
-    PasswordAuthentication no
-    IdentityFile ~/.ssh/school
-    UserKnownHostsFile ${home}/.ssh/known_hosts
-    StrictHostKeyChecking no
-"
+# Puppet manifest to configure SSH settings
+file_line { 'Turn off passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
 }
